b2354e65f0be24ea934b9bc699057fc58767f51d
Haziq Musa <last.game01987@gmail.com>
2022-11-10 05:09:17 +0800

    refactor

--- utils/checkURL.js
+++ utils/checkURL.js
@@ -1,5 +1,7 @@
 export const checkURL = url => {
-    const arrURL = url.split("api/");
-    if (arrURL.length == 2) return arrURL[1];
-    else false;
+  const arrURL = url.split("api/");
+  if (arrURL.length == 2) {
+    let split = arrURL[1].split("/");
+    return { id: split[0], name: split[1] };
+  } else false;
 };
