2d0c7b973ba36808615aac3c35f6100a825f5c52
zyq-m <last.game01987@gmail.com>
2023-11-12 03:24:33 +0800

    refactor code

--- pages/student/PayNow.js
+++ pages/student/PayNow.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, TouchableOpacity, Platform } from "react-native";
 
 import { Button } from "../../components";
@@ -6,61 +6,101 @@ import { useUserContext } from "../../hooks";
 
 import { globals, payNowStyle } from "../../styles";
 
-const PayNow = ({ navigation }) => {
-  const [active, setActive] = useState({ btn1: true, btn2: false });
+const PayNow = ({ navigation, route }) => {
+  const [page, setPage] = useState({});
+  const { loyalty } = route.params;
+
+  useEffect(() => {
+    if (!loyalty) {
+      setPage({
+        title: "Choose an amount",
+        option: [
+          {
+            id: 1,
+            name: "RM 1",
+            amount: 1,
+            active: true,
+            screen: Platform.OS === "web" ? "Cafe List" : "QR Scan",
+          },
+          {
+            id: 2,
+            name: "RM 2",
+            amount: 2,
+            active: false,
+            screen: Platform.OS === "web" ? "Cafe List" : "QR Scan",
+          },
+        ],
+      });
+    } else {
+      setPage({
+        title: "Choose a campaign",
+        option: [
+          {
+            id: 1,
+            name: "Cashless",
+            amount: 1,
+            active: true,
+            screen: "Cashless",
+          },
+          {
+            id: 2,
+            name: "Green Campus",
+            amount: 1,
+            active: false,
+            screen: "Green Campus",
+          },
+        ],
+      });
+    }
+  }, []);
 
   const onActive = (value) => {
-    if ("btn1" === value) {
-      setActive({ btn1: true, btn2: false });
-    }
-    if ("btn2" === value) {
-      setActive({ btn1: false, btn2: true });
-    }
+    setPage((prev) => {
+      const title = prev.title;
+      const option = prev.option.map((e) => {
+        let returnVal = { ...e };
+        if (e.id === value) {
+          returnVal.active = true;
+        } else {
+          returnVal.active = false;
+        }
+
+        return returnVal;
+      });
+
+      return { title, option };
+    });
   };
 
   const onRoute = () => {
-    let amount = 0;
-    if (active.btn1) {
-      amount = 1;
-    } else {
-      amount = 2;
-    }
-
-    if (Platform.OS === "web") {
-      navigation.navigate("Cafe List", { amount: amount });
-    } else {
-      navigation.navigate("QR Scan", { amount: amount });
-    }
+    page.option.forEach((e) => {
+      if (e.active) {
+        navigation.navigate(e.screen, { amount: e.amount });
+      }
+    });
   };
 
   return (
     <View style={[globals.container, { paddingHorizontal: 16 }]}>
       <View style={{ flex: 1, justifyContent: "center" }}>
         <Text style={[payNowStyle.textCenter, payNowStyle.payHeader]}>
-          Choose your amount
+          {page?.title}
         </Text>
-        <TouchableOpacity onPress={() => onActive("btn1")}>
-          <Text
-            style={[
-              payNowStyle.textCenter,
-              payNowStyle.payAmount,
-              active.btn1 && payNowStyle.active,
-            ]}
-          >
-            RM 1
-          </Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => onActive("btn2")}>
-          <Text
-            style={[
-              payNowStyle.textCenter,
-              payNowStyle.payAmount,
-              active.btn2 && payNowStyle.active,
-            ]}
-          >
-            RM 2
-          </Text>
-        </TouchableOpacity>
+        {page?.option?.map((op) => {
+          return (
+            <TouchableOpacity key={op.id} onPress={() => onActive(op.id)}>
+              <Text
+                style={[
+                  payNowStyle.textCenter,
+                  payNowStyle.payAmount,
+                  op.active && payNowStyle.active,
+                ]}
+              >
+                {op.name}
+              </Text>
+            </TouchableOpacity>
+          );
+        })}
       </View>
       <View style={{ paddingBottom: 24 }}>
         <Button label={"Next"} onPress={onRoute} />
