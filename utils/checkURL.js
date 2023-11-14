4a4eb50cef9adc2a8ece728c026c0a2ede8ee255
zyq-m <last.game01987@gmail.com>
2023-11-15 01:03:26 +0800

    update checkurl

--- utils/checkURL.js
+++ utils/checkURL.js
@@ -4,6 +4,7 @@ export function checkURL(url) {
   const otp = params.get("otp");
   const amount = params.get("amount");
   const pointId = params.get("pointId");
+  const name = params.get("name");
 
-  return { id, otp, amount, pointId };
+  return { id, otp, amount, pointId, name };
 }
