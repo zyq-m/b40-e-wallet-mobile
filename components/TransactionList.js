a48a57d875484a269a40e1f57dc9cc92bce324cb
zyq-m <last.game01987@gmail.com>
2023-11-27 00:21:12 +0800

    update accessing obj

--- components/TransactionList.js
+++ components/TransactionList.js
@@ -3,31 +3,32 @@ import { formatTime, formatDate } from "../utils/formatTime";
 
 import TransactionItem from "./TransactionItem";
 
-const TransactionList = ({ data, user, navigation, style, border }) => {
+const TransactionList = ({ data, user, navigation, style, border, params }) => {
   return (
     <>
       {data?.map((data, i) => {
         let details = {
-          sender: `${data.matricNo} - ${data.matricNo}`,
-          recipient: data.cafe.name,
+          sender: `${data.transaction.matricNo} - ${data.transaction.matricNo}`,
+          recipient: data.transaction.cafe.name,
           transactionId: data.id,
-          amount: `RM${data.amount}`,
-          date: `${formatDate(data.created_at)} at ${formatTime(
-            data.created_at
+          amount: `RM${data.transaction.amount}`,
+          date: `${formatDate(data.transaction.created_at)} at ${formatTime(
+            data.transaction.created_at
           )}`,
         };
 
         return (
           <TransactionItem
             key={i}
-            transactionId={data.id}
-            field1={data.matricNo}
-            approved={data.walletTransaction?.approved}
-            time={formatTime(data.created_at)}
-            date={formatDate(data.created_at)}
-            amount={data.amount}
+            transactionId={data.transaction.id}
+            field1={data.transaction.matricNo}
+            approved={data.transaction?.approved}
+            time={formatTime(data.transaction.created_at)}
+            date={formatDate(data.transaction.created_at)}
+            amount={data.transaction.amount}
             role={user?.role}
             noBorder={!border && i == 0}
+            params={params}
             navigate={() => {
               navigation.navigate("Transaction Details", {
                 data: details,
