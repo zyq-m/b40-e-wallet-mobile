228e0aff7a33b61be2014b339c7b1fedfb6780c6
haziq <haziq.musa02@gmail.com>
2025-01-06 08:58:43 +0800

    update transaction

--- components/FilterList.js
+++ components/FilterList.js
@@ -1,130 +1,126 @@
 import { useState } from "react";
-import { View, Text, TouchableOpacity, Platform } from "react-native";
-import { printToFileAsync } from "expo-print";
-import { shareAsync } from "expo-sharing";
+import { View, Text, TouchableOpacity } from "react-native";
 import MCIcon from "react-native-vector-icons/MaterialCommunityIcons";
 
 import Button from "./Button";
 import FilterItem from "./FilterItem";
-import DocumentTemplate from "./DocumentTemplate";
-import { useUserContext } from "../hooks";
 
 import filterStyle from "../styles/filterStyle";
 import { loginStyle } from "../styles";
 import { api } from "../services/axiosInstance";
+import dayjs from "dayjs";
 
-const FilterList = ({
-  onCollapse,
-  list,
-  onList,
-  document,
-  customDate,
-  loyalty,
-}) => {
-  const { user } = useUserContext();
-  const [date, setDate] = useState({ from: "", to: "" });
+const FilterList = ({ onCollapse, customDate, sendFilter }) => {
+	const [date, setDate] = useState({ from: "", to: "" });
+	const [list, setList] = useState([
+		{
+			id: 1,
+			label: "Today",
+			checked: true,
+			date: {
+				from: dayjs().format("YYYY-MM-DD"),
+				to: dayjs().format("YYYY-MM-DD"),
+			},
+		},
+		{
+			id: 2,
+			label: "Week",
+			checked: false,
+			date: {
+				from: dayjs().startOf("week").format("YYYY-MM-DD"),
+				to: dayjs().endOf("week").format("YYYY-MM-DD"),
+			},
+		},
+		{
+			id: 3,
+			label: "Month",
+			checked: false,
+			date: {
+				from: dayjs().startOf("month").format("YYYY-MM-DD"),
+				to: dayjs().endOf("month").format("YYYY-MM-DD"),
+			},
+		},
+	]);
 
-  const generatePDF = async () => {
-    // On iOS/android prints the given html. On web prints the HTML from the current page.
-    try {
-      if (Platform.OS === "web") {
-        alert("Generating PDF feature still in development");
-      } else {
-        const { uri } = await printToFileAsync({
-          html: DocumentTemplate(document),
-        });
-        await shareAsync(uri, { UTI: ".pdf", mimeType: "application/pdf" });
-      }
-    } catch (error) {
-      console.warn(error);
-    }
-  };
+	function onList(id) {
+		setList((prev) =>
+			prev.map((li) => {
+				if (li.checked) sendFilter(li.date);
+				return { ...li, checked: li.id === id };
+			})
+		);
+	}
 
-  function apiURL() {
-    if (loyalty) {
-      return `/student/transaction/point/${date?.from}/${date?.to}/${user.id}`;
-    }
-    if (user.role === "CAFE") {
-      return `/cafe/transaction/${date.from}/${date.to}/${user.id}`;
-    }
-    if (user.role !== "CAFE") {
-      return `/student/transaction/wallet/${date?.from}/${date?.to}/${user.id}`;
-    }
-  }
+	async function onFind() {
+		sendFilter(date);
+	}
 
-  async function onFind() {
-    try {
-      const transaction = await api.get(apiURL());
+	return (
+		<TouchableOpacity activeOpacity={0} style={filterStyle.fitlerBackDrop}>
+			<View style={filterStyle.filterContainer}>
+				<View style={[filterStyle.filterRow, { paddingTop: 10 }]}>
+					<MCIcon name="close" size={24} onPress={onCollapse} />
+					<Text style={filterStyle.filterHeader}>Sort by</Text>
+				</View>
+				<View style={{ marginTop: 10, marginBottom: 32 }}>
+					{list?.map(({ id, label, checked }) => (
+						<FilterItem
+							key={id}
+							label={label}
+							active={checked}
+							onActive={() => onList(id)}
+						/>
+					))}
 
-      customDate(transaction.data);
-    } catch (error) {
-      customDate({ data: [] });
-    }
-  }
-
-  return (
-    <TouchableOpacity
-      activeOpacity={0}
-      style={filterStyle.fitlerBackDrop}
-      // onPress={onCollapse}
-    >
-      <View style={filterStyle.filterContainer}>
-        <View style={[filterStyle.filterRow, { paddingTop: 10 }]}>
-          <MCIcon name="close" size={24} onPress={onCollapse} />
-          <Text style={filterStyle.filterHeader}>Sort by</Text>
-        </View>
-        <View style={{ marginTop: 10, marginBottom: 32 }}>
-          {list?.map(({ id, label, checked }) => (
-            <FilterItem
-              key={id}
-              label={label}
-              active={checked}
-              onActive={() => onList(id)}
-            />
-          ))}
-
-          <View style={{ flexDirection: "row", marginBottom: 14, gap: 14 }}>
-            <View style={loginStyle.inputContainer}>
-              <Text style={loginStyle.inputLabel}>From</Text>
-              <input
-                type="date"
-                style={{
-                  flex: 1,
-                  marginLeft: 4,
-                  border: "unset",
-                  outline: "none",
-                }}
-                onChange={(e) => {
-                  setDate((prev) => ({ ...prev, from: e.target.value }));
-                }}
-              />
-            </View>
-            <View style={loginStyle.inputContainer}>
-              <Text style={loginStyle.inputLabel}>To</Text>
-              <input
-                type="date"
-                style={{
-                  flex: 1,
-                  marginLeft: 4,
-                  border: "unset",
-                  outline: "none",
-                }}
-                onChange={(e) => {
-                  setDate((prev) => ({ ...prev, to: e.target.value }));
-                }}
-              />
-            </View>
-          </View>
-          <Button label="Find" onPress={onFind} />
-        </View>
-        {/* {user.role === "CAFE" && (
-          <View style={{ marginBottom: 20 }}>
-            <Button label={"Print"} onPress={generatePDF} />
-          </View>
-        )} */}
-      </View>
-    </TouchableOpacity>
-  );
+					<View
+						style={{
+							flexDirection: "row",
+							marginBottom: 14,
+							gap: 14,
+						}}
+					>
+						<View style={loginStyle.inputContainer}>
+							<Text style={loginStyle.inputLabel}>From</Text>
+							<input
+								type="date"
+								style={{
+									flex: 1,
+									marginLeft: 4,
+									border: "unset",
+									outline: "none",
+								}}
+								onChange={(e) => {
+									setDate((prev) => ({
+										...prev,
+										from: e.target.value,
+									}));
+								}}
+							/>
+						</View>
+						<View style={loginStyle.inputContainer}>
+							<Text style={loginStyle.inputLabel}>To</Text>
+							<input
+								type="date"
+								style={{
+									flex: 1,
+									marginLeft: 4,
+									border: "unset",
+									outline: "none",
+								}}
+								onChange={(e) => {
+									setDate((prev) => ({
+										...prev,
+										to: e.target.value,
+									}));
+								}}
+							/>
+						</View>
+					</View>
+					<Button label="Find" onPress={onFind} />
+				</View>
+			</View>
+		</TouchableOpacity>
+	);
 };
 
 export default FilterList;
--- pages/Transaction.js
+++ pages/Transaction.js
@@ -1,21 +1,30 @@
 import { useState, useEffect } from "react";
-import { Text, View } from "react-native";
+import { Text, View, Platform } from "react-native";
+import MaterialIcon from "react-native-vector-icons/MaterialIcons";
 
-import { Refresh, TransactionList } from "../components";
+import { Refresh, FilterList, TransactionList } from "../components";
 import { useUserContext } from "../hooks";
 
 import { globals, transactionStyle } from "../styles";
 import { api } from "../services/axiosInstance";
+import dayjs from "dayjs";
+import toMYR from "../utils/toMYR";
 
 const Transaction = ({ navigation, route }) => {
 	const { user } = useUserContext();
-	const [tf, setTf] = useState([]);
+	const [collapse, setCollapse] = useState(false);
+	const [tf, setTf] = useState({});
+	const [filter, setFilter] = useState({
+		from: dayjs().format("YYYY-MM-DD"),
+		to: dayjs().format("YYYY-MM-DD"),
+	});
 
 	useEffect(() => {
 		const controller = new AbortController();
 
 		api.get(`/transaction/${user.role}/${user.id}`, {
 			signal: controller.signal,
+			params: filter,
 		}).then((res) => {
 			setTf(res.data);
 		});
@@ -23,13 +32,41 @@ const Transaction = ({ navigation, route }) => {
 		return () => {
 			controller.abort();
 		};
+	}, [filter]);
+
+	useEffect(() => {
+		let subscribe = true;
+		const header = async () => {
+			if (subscribe) {
+				navigation.setOptions({
+					headerRight: () => (
+						<View style={transactionStyle.row}>
+							<MaterialIcon
+								name="filter-list"
+								size={25}
+								onPress={() => setCollapse(!collapse)}
+								style={
+									Platform.OS === "web" && { marginRight: 11 }
+								}
+							/>
+						</View>
+					),
+				});
+			}
+		};
+
+		header();
+
+		return () => {
+			subscribe = false;
+		};
 	}, []);
 
 	return (
 		<View style={[globals.container, {}]}>
 			<Refresh transaction={true} style={{ paddingBottom: 24 }}>
 				<TransactionList
-					data={tf}
+					data={tf?.transactions}
 					navigation={navigation}
 					user={user}
 					border={true}
@@ -37,7 +74,7 @@ const Transaction = ({ navigation, route }) => {
 					style={transactionStyle.transactionItemWrap}
 				/>
 			</Refresh>
-			{!tf.length ? (
+			{!tf?.transactions?.length ? (
 				<Text
 					style={{
 						flex: 1,
@@ -49,7 +86,67 @@ const Transaction = ({ navigation, route }) => {
 					No transactions history
 				</Text>
 			) : (
-				<></>
+				<View
+					style={{
+						paddingHorizontal: 16,
+						paddingTop: 16,
+						paddingBottom: 12,
+						borderTopLeftRadius: 9,
+						borderTopRightRadius: 9,
+						borderWidth: 1,
+						borderColor: "rgba(0, 0, 0, 0.08)",
+						backgroundColor: "#FFF",
+						flexDirection: "row",
+						justifyContent: "space-between",
+					}}
+				>
+					<View
+						style={{
+							flexDirection: "row",
+							alignItems: "baseline",
+							gap: 4,
+						}}
+					>
+						<Text
+							style={{
+								fontSize: 12,
+								fontWeight: "500",
+								color: "rgba(0, 0, 0, 0.47)",
+							}}
+						>
+							Total:
+						</Text>
+						<Text style={{ fontWeight: "500" }}>
+							{toMYR(tf?.summary.totalAmount ?? 0)}
+						</Text>
+					</View>
+					<View
+						style={{
+							flexDirection: "row",
+							alignItems: "baseline",
+							gap: 4,
+						}}
+					>
+						<Text
+							style={{
+								fontSize: 12,
+								fontWeight: "500",
+								color: "rgba(0, 0, 0, 0.47)",
+							}}
+						>
+							Transaction:
+						</Text>
+						<Text style={{ fontWeight: "500" }}>
+							{tf?.summary.totalTf}
+						</Text>
+					</View>
+				</View>
+			)}
+			{collapse && (
+				<FilterList
+					onCollapse={() => setCollapse(!collapse)}
+					sendFilter={(e) => setFilter(e)}
+				/>
 			)}
 		</View>
 	);
