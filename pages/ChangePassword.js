b09fd20a363cdd043fa775ef0ae52b4d5060dd3f
haziq <haziq.musa02@gmail.com>
2026-01-02 19:25:41 +0800

    update change password

--- pages/ChangePassword.js
+++ pages/ChangePassword.js
@@ -1,96 +1,95 @@
 import { useState } from "react";
-import { View, Text, StyleSheet, TextInput } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
-import { useUserContext } from "../hooks";
 import { Button } from "../components";
+import { useUserContext } from "../hooks";
 import { popupMessage } from "../utils/popupMessage";
 
-import { globals, loginStyle } from "../styles";
 import { changePassword } from "../api/auth/auth";
+import { globals, loginStyle } from "../styles";
 
 const ChangePassword = ({ navigation }) => {
-  const [credential, setCredential] = useState({
-    currPass: "",
-    newPass: "",
-    rePass: "",
-  });
-  const { user } = useUserContext();
+	const [credential, setCredential] = useState({
+		currPass: "",
+		newPass: "",
+		rePass: "",
+	});
+	const { user } = useUserContext();
 
-  const handleSend = async () => {
-    const { currPass, newPass, rePass } = credential;
+	const handleSend = async () => {
+		const { currPass, newPass, rePass } = credential;
 
-    try {
-      await changePassword(user.id, currPass, newPass, rePass);
-      // Clear form
-      setCredential({ currPass: "", newPass: "", rePass: "" });
-      popupMessage({
-        message: "Password changed successful",
-        title: "Success",
-      });
+		try {
+			await changePassword(user.id, currPass, newPass, rePass);
+			// Clear form
+			setCredential({ currPass: "", newPass: "", rePass: "" });
+			popupMessage({
+				message: "Password changed successful",
+				title: "Success",
+			});
 
-      navigation.navigate("Dashboard");
-    } catch (error) {
-      console.log(error);
-      popupMessage({ message: error.response.data.message, title: "Alert" });
-    }
-  };
+			navigation.navigate("Dashboard");
+		} catch (error) {
+			popupMessage({ message: error.response.data?.message, title: "Alert" });
+		}
+	};
 
-  return (
-    <View style={globals.container}>
-      <View style={{ padding: 16 }}>
-        <Para>Current password</Para>
-        <Input
-          value={credential.currPass}
-          onChangeText={(e) =>
-            setCredential((prev) => ({ ...prev, currPass: e }))
-          }
-        />
+	return (
+		<View style={globals.container}>
+			<View style={{ padding: 16 }}>
+				<Para>Current password</Para>
+				<Input
+					value={credential.currPass}
+					onChangeText={(e) =>
+						setCredential((prev) => ({ ...prev, currPass: e }))
+					}
+				/>
 
-        <Para>New Password</Para>
-        <Input
-          value={credential.newPass}
-          onChangeText={(e) =>
-            setCredential((prev) => ({ ...prev, newPass: e }))
-          }
-        />
+				<Para>New Password</Para>
+				<Input
+					value={credential.newPass}
+					onChangeText={(e) =>
+						setCredential((prev) => ({ ...prev, newPass: e }))
+					}
+				/>
 
-        <Para>Re-type password</Para>
-        <Input
-          value={credential.rePass}
-          onChangeText={(e) =>
-            setCredential((prev) => ({ ...prev, rePass: e }))
-          }
-        />
+				<Para>Re-type password</Para>
+				<Input
+					value={credential.rePass}
+					onChangeText={(e) =>
+						setCredential((prev) => ({ ...prev, rePass: e }))
+					}
+				/>
 
-        <Button label={"Change"} onPress={handleSend} />
-      </View>
-    </View>
-  );
+				<Button label={"Change"} onPress={handleSend} />
+			</View>
+		</View>
+	);
 };
 
 const Para = ({ children, style }) => {
-  return <Text style={[aboutStyle.para, style]}>{children}</Text>;
+	return <Text style={[aboutStyle.para, style]}>{children}</Text>;
 };
 
 const Input = (props) => {
-  return (
-    <TextInput
-      {...props}
-      secureTextEntry={true}
-      style={[loginStyle.inputContainer, aboutStyle.form]}
-    />
-  );
+	return (
+		<TextInput
+			{...props}
+			secureTextEntry={true}
+			style={[loginStyle.inputContainer, aboutStyle.form]}
+		/>
+	);
 };
 
 const aboutStyle = StyleSheet.create({
-  para: {
-    marginLeft: 6,
-    marginBottom: 6,
-  },
-  form: {
-    marginTop: 0,
-    marginBottom: 24,
-  },
+	para: {
+		marginLeft: 6,
+		marginBottom: 6,
+	},
+	form: {
+		marginTop: 0,
+		marginBottom: 24,
+	},
 });
 
 export default ChangePassword;
